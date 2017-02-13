f52d1ad0e3f55b11437b2b5ce41f257d0a69fb74
BJTTTT <bjguozhitao@icloud.com>
2017-02-13 01:19:15 -0800

    NA

--- src/helper/log.erl
+++ src/helper/log.erl
@@ -1,40 +1,83 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% log.erl
+% mslog.erl
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--module(log).
+-module(mslog).
 
 -include("../include/header_const.hrl").
 -include("../include/header_struct.hrl").
 
--export([log_process/0,
-         log_all/1,
+-export([log_all/1,
+         log_all/3,
          log_info/1,
+         log_info/3,
          log_warn/1,
-         log_err/1]).
+         log_warn/3,
+         log_err/1,
+         log_err/3]).
+%         log_vdr_statistics_info/2,
+%         log_vdr_info/3,
+%         log_vdr_info/4,
+%         save_msg_4_vdr/3]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Description:
-%   Log process will receive all log request and log the messages.
+% Description :
+%   Debug function. Save messages between the VDR and the gateway to disk.
 % Parameter :
-%       Log         : YES/NO, should be the system defined display log enabled
-%       LogLevel    : DISP_LEVEL_ALL/DISP_LEVEL_INFO/DISP_LEVEL_WARN/DISP_LEVEL_ERR, should be the system defined display log level
-% Return :
-%       ok
-% 
+%       State   :
+%       FromVDR :
+%       Msg     :
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-log_process(Level, Log, LogLevel) ->
-    receive
-        {Format} ->
-            case Level of
-                ?DISP_LEVEL_ALL ->
-                    log_all(Format, Log, LogLevel),
-                    log_process
-                    
-            
+save_msg_4_vdr(State, FromVDR, Msg) ->
+    VDRID = State#vdritem.id,
+    StoredMsg = State#vdritem.storedmsg4save,
+    VDRLogPid = State#vdritem.vdrlogpid,
+    if
+        VDRID == undefined ->
+            {Year,Month,Day} = erlang:date(),
+            {Hour,Min,Second} = erlang:time(),
+            NewMsg = [{FromVDR, Msg, Year, Month, Day, Hour, Min, Second}],
+            NewStoredMsg = lists:merge([StoredMsg, NewMsg]),
+            mslog:log_vdr_info(none, State, "vdr_handler:save_msg_4_vdr(...) store data : ~p", NewStoredMsg),
+            State#vdritem{storedmsg4save=NewStoredMsg};
+        true ->
+            if
+                StoredMsg =/= [] ->
+                    mslog:log_vdr_info(none, State, "vdr_handler:save_msg_4_vdr(...) send stored data : ~p", StoredMsg),
+                    save_stored_msg_4_vdr(VDRID, StoredMsg, VDRLogPid);
+                true ->
+                    ok
+            end,
+            {Year,Month,Day} = erlang:date(),
+            {Hour,Min,Second} = erlang:time(),
+            DateTime = {Year, Month, Day, Hour, Min, Second},
+            mslog:log_vdr_info(none, State, "vdr_handler:save_msg_4_vdr(...) send data : ~p", Msg),
+            VDRLogPid ! {save, VDRID, FromVDR, Msg, DateTime},
+            State#vdritem{storedmsg4save=[]}
+    end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Description :
+%   Send messages to the message saving process.
+% Parameter :
+%       Pid, 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+save_stored_msg_4_vdr(VDRID, StoredMsg, VDRLogPid) when VDRLogPid =/= undefined,
+                                                        is_list(StoredMsg),
+                                                        length(StoredMsg) > 0 ->
+    [H|T] = StoredMsg,
+    {FromVDR, MsgBin, Year, Month, Day, Hour, Min, Second} = H,
+    DateTime = {Year, Month, Day, Hour, Min, Second},
+    VDRLogPid ! {save, VDRID, FromVDR, MsgBin, DateTime},
+    save_stored_msg_4_vdr(VDRID, T, VDRLogPid);
+save_stored_msg_4_vdr(_VDRID, _StoredMsg, _VDRLogPid) ->
+    ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -322,38 +365,33 @@ log_all(Format, Data) when is_list(Data) ->
             error_logger:error_msg("log_all(Format - string, Data - binary) parameter error : " ++ erlang:get_stacktrace())
     end;
 log_all(_Format, _Data) ->
-    error_logger:error_msg("do_log(Format, Data) parameter error : " ++ erlang:get_stacktrace()).
+    ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Description:
 %   Log all messages.
 % Parameter :
-%       Format + Data   : binary/list to be displayed
+%       Data        : binary/list to be displayed
 %       Log         : boolean true/false, should be the system defined display log enabled
 %       LogLevel    : DISP_LEVEL_ALL/DISP_LEVEL_INFO/DISP_LEVEL_WARN/DISP_LEVEL_ERR, should be the system defined display log level
 % Return :
 %       ok
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-log_all(Format, Data, Log, LogLevel) when is_binary(Data),
-                                          Log >= ?NO,
-                                          Log =< ?YES,
-                                          LogLevel >= ?DISP_LEVEL_ALL,
-                                          LogLevel =< ?DISP_LEVEL_ERR ->
-    if
-        string_helper:is_string(Format) =:= ?YES ->
-            do_log(Format, Data, ?DISP_LEVEL_ALL, Log, LogLevel);
-        true ->
-            error_logger:error_msg("log_all(Format - string, Data - binary, Log, LogLevel) parameter error : " ++ erlang:get_stacktrace())
-    end;
-log_all(Format, Data, Log, LogLevel) when is_list(Data),
-                                          Log >= ?NO,
-                                          Log =< ?YES,
-                                          LogLevel >= ?DISP_LEVEL_ALL,
-                                          LogLevel =< ?DISP_LEVEL_ERR ->
-    do_log(Format, Data, ?DISP_LEVEL_ALL, Log, LogLevel);
-log_all(_Format, _Data, _Log, _LogLevel) ->
+log_all(Data, Log, LogLevel) when is_binary(Data),
+                                 Log >= ?NO,
+                                 Log =< ?YES,
+                                 LogLevel >= ?DISP_LEVEL_ALL,
+                                 LogLevel =< ?DISP_LEVEL_ERR ->
+    do_log(Data, ?DISP_LEVEL_ALL, Log, LogLevel);
+log_all(Data, Log, LogLevel) when is_list(Data),
+                                 Log >= ?NO,
+                                 Log =< ?YES,
+                                 LogLevel >= ?DISP_LEVEL_ALL,
+                                 LogLevel =< ?DISP_LEVEL_ERR ->
+    do_log(Data, ?DISP_LEVEL_ALL, Log, LogLevel);
+log_all(_Data, _Log, _LogLevel) ->
     ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -378,7 +416,7 @@ log_info(_Data) ->
 % Description:
 %   Log information messages.
 % Parameter :
-%       Data        : binary/list to be displayed
+%       Data        :
 %       Log         : YES/NO, should be the system defined display log enabled
 %       LogLevel    : DISP_LEVEL_ALL/DISP_LEVEL_INFO/DISP_LEVEL_WARN/DISP_LEVEL_ERR, should be the system defined display log level
 % Return :
@@ -488,3 +526,71 @@ log_err(Data, Log, LogLevel) when is_list(Data),
 log_err(_Data, _Log, _LogLevel) ->
     ok.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Description :
+%   VDR sends information to link information process.
+% Parameter :
+%       State   :
+%       Type    :
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+log_vdr_statistics_info(State, Type) ->
+    if
+        State#vdritem.conninfopid =/= undefined ->
+            State#vdritem.conninfopid ! {self(), Type},
+            ok;
+        true ->
+            log_vdr_info(error, State, "undefined link info pid")
+    end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Description :
+% Parameter :
+%       Type        : all|none|info|hint|error
+%       State       :
+%       FormatEx    :
+%       DataEx      :
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+log_vdr_info(Type, State, FormatEx) when is_list(FormatEx) ->
+    log_vdr_info(Type, State, FormatEx).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Description :
+% Parameter :
+%       Type        : all|none|info|hint|error
+%       State       :
+%       FormatEx    :
+%       DataEx      :
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+log_vdr_info(Type, State, FormatEx, DataEx) when is_list(FormatEx),
+                                                 is_list(DataEx) ->
+    Format = "(PID ~p, id ~p, addr ~p, serialno ~p, authcode ~p, vehicleid ~p, vehiclecode ~p, driverid ~p)~n",
+    NewFormat = string:concat(Format, FormatEx),
+    Data = [self(),
+            State#vdritem.id, 
+            State#vdritem.addr,
+            State#vdritem.serialno,
+            State#vdritem.auth,
+            State#vdritem.vehicleid,
+            State#vdritem.vehiclecode,
+            State#vdritem.driverid],
+    NewData = lists:append(Data, DataEx),
+    if
+        Type == error ->
+            log:logerr(NewFormat, NewData);
+        Type == hint ->
+            log:loghint(NewFormat, NewData);
+        Type == info ->
+            log:loginfo(NewFormat, NewData);
+        Type == none ->
+            log:lognone(NewFormat, NewData);
+        true ->
+            log:logall(NewFormat, NewData)
+    end;
+log_vdr_info(_Type, _State, _FormatEx, _DataEx) ->
+    ok.
